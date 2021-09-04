b7dd78f13ba9c867c6733a09e3618233e447050f
Koulinn <drdverzola@gmail.com>
2021-09-04 14:28:11 +0100

    upload experience image working properly
    also used prettier

--- src/services/profile/profile-experience-handlers.js
+++ src/services/profile/profile-experience-handlers.js
@@ -4,8 +4,8 @@ import Profile from "../../db/models/Profile.js"
 
 const getExperiences = async (req, res, next) => {
   try {
-    const {_id} = req.params
-    const experiences = await Profile.findById({_id}, {experience: 1})
+    const { _id } = req.params
+    const experiences = await Profile.findById({ _id }, { experience: 1 })
     res.status(200).send(experiences.experience)
   } catch (error) {
     next(error)
@@ -15,52 +15,49 @@ const getExperiences = async (req, res, next) => {
 
 const addNewExperience = async (req, res, next) => {
   try {
-    const {_id} = req.params
-    const getUser = await Profile.findByIdAndUpdate({_id}, {$push: {experience: {...req.body, user: _id }}}, {new: true})
+    const { _id } = req.params
+    const getUser = await Profile.findByIdAndUpdate({ _id }, { $push: { experience: { ...req.body, user: _id } } }, { new: true })
 
-    res.send(getUser.experience[getUser.experience.length -1])
+    res.send(getUser.experience[getUser.experience.length - 1])
   } catch (error) {
     next(error)
   }
 }
 
+// without projection
 const update = async (req, res, next) => {
   try {
     const { _id, _userId } = req.params
-    await Profile.findOneAndUpdate({_id: _userId, "experience._id" : _id}, 
+    await Profile.findOneAndUpdate({ _id: _userId, "experience._id": _id },
       {
-        "experience.$" : {
+        "experience.$": {
           ...req.body,
           _id: _id
         }
       }
     )
 
-    const getUpdatedExperience = await Profile.findOne({_id: _userId, "experience._id": _id}, {"experience.$": 1})
-    
+    const getUpdatedExperience = await Profile.findOne({ _id: _userId, "experience._id": _id }, { "experience.$": 1 })
 
     res.send(getUpdatedExperience.experience[0])
   } catch (error) {
     next(error)
   }
 }
-
+//with projection
 const uploadImage = async (req, res, next) => {
   try {
-     const { _id, _userId } = req.params
-    await Profile.findOneAndUpdate({_id: _userId, "experience._id" : _id}, 
+    const { _id, _userId } = req.params
+    
+    const updateExperience = await Profile.findOneAndUpdate({ _id: _userId, "experience._id": _id },
       {
-        "experience.$" : {
-          image: req.file.path,
-          _id: _id
-        }
+        $set: { "experience.$.image": req.file.path }
+      },{
+        projection: { "experience.$": 1 }
       }
     )
-    
-    const getUpdatedExperience = await Profile.findOne({_id: _userId, "experience.id": _id}, {"experience.$": 1})
 
-    res.send(getUpdatedExperience.experience[0])
-    
+    res.send(updateExperience.experience[0])
   } catch (error) {
     next(error)
   }
