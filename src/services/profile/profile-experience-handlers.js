ea1382a3f2efd34dbe84b0d2fafa38e53ac8be95
Koulinn <drdverzola@gmail.com>
2021-09-04 13:48:43 +0100

    fixed post add new experience and created upload image fucntion

--- src/services/profile/profile-experience-handlers.js
+++ src/services/profile/profile-experience-handlers.js
@@ -16,7 +16,7 @@ const getExperiences = async (req, res, next) => {
 const addNewExperience = async (req, res, next) => {
   try {
     const {_id} = req.params
-    const getUser = await Profile.findByIdAndUpdate({_id}, {$push: {experience: req.body}}, {new: true})
+    const getUser = await Profile.findByIdAndUpdate({_id}, {$push: {experience: {...req.body, user: _id }}}, {new: true})
 
     res.send(getUser.experience[getUser.experience.length -1])
   } catch (error) {
@@ -27,21 +27,16 @@ const addNewExperience = async (req, res, next) => {
 const update = async (req, res, next) => {
   try {
     const { _id, _userId } = req.params
-    const updateExperience = await Profile.findOneAndUpdate({_id: _userId, "experience._id" : _id}, 
-    {
-      
-      "experience.$" : {
-        ...req.body,
-        _id: _id
+    await Profile.findOneAndUpdate({_id: _userId, "experience._id" : _id}, 
+      {
+        "experience.$" : {
+          ...req.body,
+          _id: _id
+        }
       }
-    })
+    )
 
     const getUpdatedExperience = await Profile.findOne({_id: _userId, "experience.id": _id}, {"experience.$": 1})
-
-    
-    // delete updatedProfile._doc.password
-    // delete updatedProfile._doc.experience
-    // delete updatedProfile._doc.email
 
     res.send(getUpdatedExperience.experience[0])
   } catch (error) {
@@ -51,16 +46,19 @@ const update = async (req, res, next) => {
 
 const uploadImage = async (req, res, next) => {
   try {
-    const { _id } = req.params
-    const updatedProfile = await Profile.findByIdAndUpdate(_id, {image: req.file.path}, {
-      new: true
-    })
+     const { _id, _userId } = req.params
+    await Profile.findOneAndUpdate({_id: _userId, "experience._id" : _id}, 
+      {
+        "experience.$" : {
+          image: req.file.path,
+          _id: _id
+        }
+      }
+    )
     
-    delete updatedProfile._doc.password
-    delete updatedProfile._doc.experience
-    delete updatedProfile._doc.email
-    
-    res.status(200).send(updatedProfile)
+    const getUpdatedExperience = await Profile.findOne({_id: _userId, "experience.id": _id}, {"experience.$": 1})
+
+    res.send(getUpdatedExperience.experience[0])
     
   } catch (error) {
     next(error)
