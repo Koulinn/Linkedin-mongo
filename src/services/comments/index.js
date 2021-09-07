b1aa8fed4b3d2f85505cc891e0c88017874e8fef
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-07 08:49:56 +0200

    routes

--- src/services/comments/index.js
+++ src/services/comments/index.js
@@ -1,5 +1,37 @@
 import { Router } from "express";
+import commentModel from "../../db/models/comments.js";
 
 const commentRouter = Router();
+
+commentRouter.post("/", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+commentRouter.get("/", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+commentRouter.get("/:Id", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+commentRouter.put("/:Id", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
+commentRouter.delete("/:Id", async (req, res, next) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default commentRouter;
