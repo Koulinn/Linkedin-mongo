6b96ffdec9311c255032760fd7a3fb51493af2da
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-09-09 17:32:53 +0200

    index

--- src/services/post/index.js
+++ src/services/post/index.js
@@ -4,30 +4,20 @@ import cloudStorage from "../../lib/cloud-storage.js";
 import post from "./post-handlers.js";
 
 const postRouter = Router();
+//refactored
+postRouter.route("/").post(post.postAPost).get(post.getPosts);
 
-
-postRouter.route("/")
-  .post(post.postAPost)
-  .get(post.getPosts)
-
-
-
-postRouter.route("/:Id")
+postRouter
+  .route("/:Id")
   .get(post.getById)
   .put(post.updatePost)
   .delete(post.deletePost)
-  .post(multer({ storage: cloudStorage }).single("image"), post.postImage)
+  .post(multer({ storage: cloudStorage }).single("image"), post.postImage);
 
-
-postRouter.route("/post/:id").post(post.postComment)
-postRouter.route("/post/:id/comments").get(post.getPostComments)
-postRouter.route("/post/:id/comments/:commentId").get(post.getCommentAndPost)
-postRouter.route("/post/:id/comments/:commentId").put(post.updateComment)
-postRouter.route("/post/:id/comments/:commentId").delete(post.deleteComment)
-
-
-
+postRouter.route("/post/:id").post(post.postComment);
+postRouter.route("/post/:id/comments").get(post.getPostComments);
+postRouter.route("/post/:id/comments/:commentId").get(post.getCommentAndPost);
+postRouter.route("/post/:id/comments/:commentId").put(post.updateComment);
+postRouter.route("/post/:id/comments/:commentId").delete(post.deleteComment);
 
 export default postRouter;
-
-
